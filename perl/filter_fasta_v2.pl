c6b0deb820a531d9a6d8dadd34b9fa3fd502557b
rob123king <rob123king@users.noreply.github.com>
2015-07-23 15:31:19 +0100

    Create filter_fasta_v2.pl
    Perl script to filter fasta file using scaffold ID's

---
+++ perl/filter_fasta_v2.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+##RUN BY "./filter_fasta_v2.pl > filtered_fa.fasta"
+
+#CHANGE THESE TO MATCH YOUR FILES
+my $idsfile = "ids.txt";
+my $seqfile = "seqs.fa";
+
+
+my %ids  = ();
+
+open FILE, $idsfile;
+while(<FILE>) {
+  chomp;
+  $ids{$_} += 1;
+}
+close FILE;
+
+local $/ = "\n>";  # read by FASTA record
+
+open FASTA, $seqfile;
+while (<FASTA>) {
+    chomp;
+    my $seq = $_;
+    my ($id) = $seq =~ /^>*(\S+)/;  # parse ID as first word in FASTA header
+    if (exists($ids{$id})) {
+        $seq =~ s/^>*.+\n//;  # remove FASTA header
+        $seq =~ s/\n//g;  # remove endlines
+        print "$seq\n";
+    }
+}
+close FASTA;
