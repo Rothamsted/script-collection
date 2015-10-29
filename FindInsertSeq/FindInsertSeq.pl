08c0bb1a7e301761e790c3bfddcc1dc426cb4f15
rob123king <rob123king@users.noreply.github.com>
2015-10-29 10:06:41 +0000

    Update FindInsertSeq.pl
    quick fix for novoalign switch on last step bug

--- FindInsertSeq/FindInsertSeq.pl
+++ FindInsertSeq/FindInsertSeq.pl
@@ -84,14 +84,20 @@ close(READS);
 
 print "+++++ STEP 2 +++++\n";
 #take unmapped reads from mapped pair reads and convert to fastq, map to full reference, convert, sort, index.
+
+if ($mapper=~"bowtie") {
 foreach my $SAM (@SAMdata){
 	print "samtools view -h -S -f 5 -F 8 $SAM > unmapped_$SAM\n";
 	system("samtools view -h -S -f 5 -F 8 $SAM > unmapped_$SAM") == 0  or die "system samtools take unmapped reads: $?";
 	print "SAM To FASTQ: cat unmapped_$SAM | grep... | awk... > unmapped_$SAM.fastq\n";
 	system("cat unmapped_$SAM | grep -v ^\@ | awk '{print \"\@\"\$1\"\\n\"\$10\"\\n+\\n\"\$11\}' > unmapped_$SAM.fastq ") == 0  or die "system samtools convert unmapped reads to Fastq: $?";
 	#FASTQ file contains mixed R1/R2 reads so not all are paired in one fastq file. BWA does not seem to keep te orientation of the mapped reads in this format which is assumed why it was unsuccessful identifying some plasmid insertions. BOWTIE2 and Novoalign do kepp orientation of the reads and do identify insertion site for all samples tested
-	print "bowtie2 -x genome.ndx -U unmapped_$SAM.fastq -S unmapped_$SAM.sam\n";
+	
+
+    print "bowtie2 -x genome.ndx -U unmapped_$SAM.fastq -S unmapped_$SAM.sam\n";
 	system("bowtie2 -x genome.ndx -U unmapped_$SAM.fastq -S unmapped_$SAM.sam") == 0  or die "system Bowtie2: $?";
+
+	
 	print "samtools view -uhS unmapped_$SAM.sam | samtools sort - unmapped_$SAM.sort\n";
 	system("samtools view -uhS unmapped_$SAM.sam | samtools sort - unmapped_$SAM.sort") == 0  or die "system samtools sam to bam: $?";
 	#print "samtools sort unmapped_$SAM.bam unmapped_$SAM.sort\n";
@@ -135,9 +141,64 @@ foreach my $SAM (@SAMdata){
 		#Total ordered mapped reads for user to eyeball location quickly for tablet view and for problem checks
 		print $a . "\t" . $Chrom{$name} . "\n";	
 	}
-
+}
 }
 
+if ($mapper=~"novoalign") {
+foreach my $SAM (@SAMdata){
+	print "samtools view -h -S -f 5 -F 8 $SAM > unmapped_$SAM\n";
+	system("samtools view -h -S -f 5 -F 8 $SAM > unmapped_$SAM") == 0  or die "system samtools take unmapped reads: $?";
+	print "SAM To FASTQ: cat unmapped_$SAM | grep... | awk... > unmapped_$SAM.fastq\n";
+	system("cat unmapped_$SAM | grep -v ^\@ | awk '{print \"\@\"\$1\"\\n\"\$10\"\\n+\\n\"\$11\}' > unmapped_$SAM.fastq ") == 0  or die "system samtools convert unmapped reads to Fastq: $?";
+	#FASTQ file contains mixed R1/R2 reads so not all are paired in one fastq file. BWA does not seem to keep te orientation of the mapped reads in this format which is assumed why it was unsuccessful identifying some plasmid insertions. BOWTIE2 and Novoalign do kepp orientation of the reads and do identify insertion site for all samples tested
+	
+
+    print "bowtie2 -x genome.ndx -U unmapped_$SAM.fastq -S unmapped_$SAM.sam\n";
+	system("bowtie2 -x genome.ndx -U unmapped_$SAM.fastq -S unmapped_$SAM.sam") == 0  or die "system Bowtie2: $?";
+
+	
+	print "samtools view -uhS unmapped_$SAM.sam | samtools sort - unmapped_$SAM.sort\n";
+	system("samtools view -uhS unmapped_$SAM.sam | samtools sort - unmapped_$SAM.sort") == 0  or die "system samtools sam to bam: $?";
+	#print "samtools sort unmapped_$SAM.bam unmapped_$SAM.sort\n";
+	#system("samtools sort unmapped_$SAM.bam unmapped_$SAM.sort") == 0  or die "system samtools sort: $?";
+	print "samtools index unmapped_$SAM.sort.bam\n";
+	system("samtools index unmapped_$SAM.sort.bam") == 0  or die "system samtools index: $?";
+
+	#create mpileup file for peak detection 
+	print "samtools mpileup -BQ 0 unmapped_$SAM.sort.bam > unmapped_$SAM.sort.bam.mpileup\n";
+	system("samtools mpileup -BQ 0 unmapped_$SAM.sort.bam > unmapped_$SAM.sort.bam.mpileup") == 0  or die "system samtools mpileup: $?";
+	
+	print "\n****PAIRED READ SET $SAM COMPLETED****\n\n";
+
+	#hash
+	my %Chrom;
+	#used to make key unique
+	my $counter=0;
+
+	open (READS, "<unmapped_$SAM.sam");
+	while (<READS>) {
+		chomp;
+		if($_ =~ /^@/){
+			next;
+		}
+		#split columns by tabs
+		my @col = split /\t/;
+
+		#key is made unique by addition to name of counter and both key and value added to hash (probably simple way of doing this but works fine)
+		#don't include unmapped reads
+		if ($col[3] != 0){
+			$Chrom{$col[2].":".$counter} = $col[3];
+			$counter++;
+		}
+	}
+	close(READS);	
 
 
-
+	#print the hash to screen
+	foreach my $name (sort { $Chrom{$a} <=> $Chrom{$b} or $a cmp $b } keys %Chrom) {    
+		my ($a,$b)=split(/:/, $name);
+		#Total ordered mapped reads for user to eyeball location quickly for tablet view and for problem checks
+		print $a . "\t" . $Chrom{$name} . "\n";	
+	}
+}
+}
